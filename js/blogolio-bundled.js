b54ffd80ba6a3daed465d50f7cd9a2c8e40b5340
timo.obereder <timo.obereder@ebcont.com>
2015-11-23 08:26:06 +0100

    change: index.html, blogolio-bundled.js - quick fix

--- js/blogolio-bundled.js
+++ js/blogolio-bundled.js
@@ -13,15 +13,35 @@ $(function () {
 
 	var blogs = new Blogs();
 
+	var BlogsView = Parse.View.extend({
+		template: Handlebars.compile($('#blogs-tpl').html()),
+		render: function () {
+			var collection = { blog: this.collection.toJSON() };
+			this.$el.html(this.template(collection));
+		}
+	});
+
 	blogs.fetch({
 		success: function (blogs) {
 			console.log(blogs);
+			var blogsView = new BlogsView({ collection: blogs });
+			blogsView.render();
+			$('.main-container').html(blogsView.el);
 		},
 		error: function (blogs, error) {
 			console.log(error);
 		}
 	});
 });
+
+// index.js
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+// ReactDOM.render(
+// <h1>Hello, world!</h1>,
+// document.getElementById('example')
+// );
 
 // $(function () {
 
