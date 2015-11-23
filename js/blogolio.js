67222f2662646629be923a1df274a4bf8d65ed6d
timo.obereder <timo.obereder@ebcont.com>
2015-11-23 08:01:57 +0100

    change: index.html, blogolio.js for blog post test.

--- js/blogolio.js
+++ js/blogolio.js
@@ -12,9 +12,20 @@ $(function() {
 	
 	var blogs = new Blogs();
 	
+	var BlogsView = Parse.View.extend({
+	    template: Handlebars.compile($('#blogs-tpl').html()),
+	    render: function(){ 
+	        var collection = { blog: this.collection.toJSON() };
+	        this.$el.html(this.template(collection));
+	    }
+	});
+
 	blogs.fetch({
 		success: function(blogs) {
 			console.log(blogs);
+			var blogsView = new BlogsView({ collection: blogs });
+		    blogsView.render();
+		    $('.main-container').html(blogsView.el);
 		},
 		error: function(blogs, error){
 			console.log(error);
