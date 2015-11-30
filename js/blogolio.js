3b0938c53189343d31bc34c93e3049a81d2ed8e7
timo.obereder <timo.obereder@ebcont.com>
2015-11-30 14:55:24 +0100

    add: quick fix hopefully

--- js/blogolio.js
+++ js/blogolio.js
@@ -136,7 +136,7 @@ $(function() {
 			render: function() {
 				var collection = { 
 					username: this.options.username,
-					blog: this.collection
+					blog: this.collection.toJSON()
 				};
 				this.$el.html(this.template(collection));
 			}
@@ -146,7 +146,7 @@ $(function() {
 			render: function() {
 				var collection = { 
 					username: this.options.username,
-					project: this.collection
+					project: this.collection.toJSON() 
 				};
 				this.$el.html(this.template(collection));
 			}
