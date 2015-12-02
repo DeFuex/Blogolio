0201c8a17e83cf81a1c52972a72fa8246cd61ba4
timo.obereder <timo.obereder@ebcont.com>
2015-12-02 09:11:04 +0100

    add: test fetch function

--- js/blogolio.js
+++ js/blogolio.js
@@ -168,7 +168,7 @@ $(function() {
 			initialize: function(){
 				this.blogs = new Blogs();
 
-				this.listenTo(this.blogs, 'reset', this.render);
+				this.on(this.blogs, 'reset', this.render);
 
 				this.fetch1 = this.blogs.fetch({
 					reset: true
