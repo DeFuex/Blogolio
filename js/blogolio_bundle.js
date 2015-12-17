fadb45d9e64cfbd8dc420a498e31d2782c9a7cbd
timo.obereder <timo.obereder@ebcont.com>
2015-12-17 14:56:39 +0100

    change: tinyMCE init settings

--- js/blogolio_bundle.js
+++ js/blogolio_bundle.js
@@ -521,13 +521,22 @@ $(function() {
 			});
 		});
 
-		tinymce.remove();
+		
       	tinymce.init({
+      		setup: function(e){
+      			e.on('init', function(args) {
+      				console.debug(args.target.id);
+      			})
+      		},
 	        selector: "textarea.form-control",
 	        plugins: [
-	          "media table paste "
+	        	"media table paste "
 	        ],
-	        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter      alignright alignjustify | bullist numlist outdent indent | link image"
+	        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter      alignright alignjustify | bullist numlist outdent indent | link image",
+			content_css: [
+				'//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+				'//www.tinymce.com/css/codepen.min.css'
+			]
 	    });
 	});
 });
