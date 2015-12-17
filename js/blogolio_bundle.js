04d06038f03eb15d12075f57c737fc01b2c3e7b1
timo.obereder <timo.obereder@ebcont.com>
2015-12-17 16:09:45 +0100

    change: tinyMCE test

--- js/blogolio_bundle.js
+++ js/blogolio_bundle.js
@@ -532,6 +532,25 @@ $(function() {
 			buttonBlog.addEventListener ("click", loadTinyMCE, false);
 		};
 
+		function loadTinyMCE() {
+	    tinymce.init({
+	  		setup: function(e){
+	  			e.on('init', function(args) {
+	  				console.debug(args.target.id);
+	  			})
+	  		},
+	        selector: "textarea.form-control",
+	        plugins: [
+	        	"media table paste "
+	        ],
+	        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter      alignright alignjustify | bullist numlist outdent indent | link image",
+			content_css: [
+				'//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+				'//www.tinymce.com/css/codepen.min.css'
+			]
+   		});
+	} 
+
 
 		// $("#addBlogButton").on("click", function(e) {
 		// 	console.log("addbutton clicked");
