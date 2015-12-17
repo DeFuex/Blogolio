d2e179b642b437b58f129706f295e3f1a703465e
timo.obereder <timo.obereder@ebcont.com>
2015-12-17 16:38:52 +0100

    change: call to init tinyMCE function

--- js/blogolio.js
+++ js/blogolio.js
@@ -156,7 +156,6 @@ $(function() {
 					blog: this.collection.toJSON()
 				};
 				this.$el.html(this.template(collection));
-				loadTinyMCE();
 			}
 		}),		
 		ProjectsAdminView = Parse.View.extend({
@@ -167,7 +166,6 @@ $(function() {
 					project: this.collection.toJSON()
 				};
 				this.$el.html(this.template(collection));
-				loadTinyMCE();
 			}
 		}),
 		WriteBlogView = Parse.View.extend({
@@ -402,8 +400,10 @@ $(function() {
 				if (!Parse.User.current()) {
 					this.navigate('#/login', { trigger: true });
 				} else {
+					loadTinyMCE();
 					var writeBlogView = new WriteBlogView();
 					writeBlogView.render();
+					
 					$container.html(writeBlogView.el);
 				}
 			},
