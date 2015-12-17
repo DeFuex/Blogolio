687c72b8640ac5bdf154c56fc77e1dd8d1e21a7f
timo.obereder <timo.obereder@ebcont.com>
2015-12-17 16:55:12 +0100

    add: quick fix

--- js/blogolio.js
+++ js/blogolio.js
@@ -537,11 +537,11 @@ $(function() {
 	        plugins: [
 	        	"media table paste "
 	        ],
-	        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter      alignright alignjustify | bullist numlist outdent indent | link image",
-			content_css: [
-				'//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-				'//www.tinymce.com/css/codepen.min.css'
-			]
+	        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter      alignright alignjustify | bullist numlist outdent indent | link image"
+			// content_css: [
+			// 	'//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+			// 	'//www.tinymce.com/css/codepen.min.css'
+			// ]
    		});
 	}
 
