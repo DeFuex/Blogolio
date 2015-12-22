639e2ebaed02c79c95ed3d332b685c5e1eb91cd7
timo.obereder <timo.obereder@ebcont.com>
2015-12-22 17:05:30 +0100

    add: browserify quick fix

--- js/blogolio_bundle.js
+++ js/blogolio_bundle.js
@@ -497,6 +497,21 @@ $(function() {
 
 		
 	$(document).ready(function(){
+
+		$("body").css("display", "none");
+	 
+	    $("body").fadeIn(2000);
+	 
+	    $("a.transition").click(function(event){
+	        event.preventDefault();
+	        linkLocation = this.href;
+	        $("body").fadeOut(1000, redirectPage);      
+	    });
+	         
+	    function redirectPage() {
+	        window.location = linkLocation;
+	    }
+		
 		var CommentObject = Parse.Object.extend("CommentObject");
 	
 		$("#contactForm").on("submit", function(e) {
