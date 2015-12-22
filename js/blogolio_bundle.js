cc663511209d6c3b5e85ba45e37e1d41203ab228
timo.obereder <timo.obereder@ebcont.com>
2015-12-22 17:19:23 +0100

    add: quick fix

--- js/blogolio_bundle.js
+++ js/blogolio_bundle.js
@@ -498,20 +498,21 @@ $(function() {
 		
 	$(document).ready(function(){
 
-		// $("body").css("display", "none");
+		$("body").css("display", "none");
 	 
 	    $("body").fadeIn(2000);
 	 
 	    $("a.transition").click(function(event){
 	        event.preventDefault();
 	        linkLocation = this.href;
+	        console.log("TEST" + linkLocation);
 	        $("body").fadeOut(1000, redirectPage);      
 	    });
 	         
 	    function redirectPage() {
 	        window.location = linkLocation;
 	    }
-		
+
 		var CommentObject = Parse.Object.extend("CommentObject");
 	
 		$("#contactForm").on("submit", function(e) {
