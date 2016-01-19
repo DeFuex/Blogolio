212c7951aa82e19e255391670261871e0f12b78a
Timo Obereder <timo.obereder@gmail.com>
2016-01-20 00:02:02 +0100

    fix: props key names for blogs and projects in render() fixed

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -33,12 +33,12 @@ export default class Projects extends ParseComponent {
 	  						console.log(img);
 
 	  						return (
-        					<div className="col-lg-12">
+        					<div className="col-lg-12" key={"blog-post-" + p.title}>
 								<h2 className="page-header" ><Link to={'/project/' + p.objectId}>{p.title}</Link></h2>
           						<div className="col-lg-3 col-md-4 col-xs-6 thumb">
-	              					<a className="thumbnail" to={'/project/' + p.objectId }>
-	                					<img className="img-responsive" src={ img } alt="" />
-	              					</a>
+	              					<Link className="thumbnail" to={'/project/' + p.objectId }>
+	                					<img className="img-responsive" src={ img }  alt="" />
+	              					</Link>
           						</div>
   		    					<p>{p.summary}</p>
         					</div>
