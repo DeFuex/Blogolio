cc4821360934d2fd7cd51e2e6f8e08d269fbf75a
timo.obereder <timo.obereder@ebcont.com>
2016-04-12 22:42:47 +0200

    add: new roboto font, changed header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,7 @@ export default class Header extends Component {
   render() {
       return (
   		<div className="blog-header">
-  			<h1><a className="blog-title" href="./">The Very Personal Blogolio</a></h1>
-  			<p className="lead blog-description">This is a portfolio blog about some projects i did.</p>
+      <img src="src/assets/header.jpg" height="180" />
   		</div>
   	)
   }
