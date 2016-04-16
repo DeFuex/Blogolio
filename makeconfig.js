2dc7cc952f9d802c5d04b5926932353588bc5784
timo.obereder <timo.obereder@ebcont.com>
2016-04-16 06:17:33 +0200

    change: public path to corresponding gh-page adress /Blogolio/

--- makeconfig.js
+++ makeconfig.js
@@ -75,7 +75,7 @@ module.exports = function(options){
 			entry: entry,
 			output: {
 				path: path.resolve(__dirname, 'build'),
-				publicPath: '/',
+				publicPath: '/Blogolio/',
 				filename: 'static/bundle.js'
 			},
 			module: {
